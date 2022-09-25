2bf2879add63e5a32c7b1749e602cee57a5f7ca8
RiteshBharvey <99973928+RiteshBharvey@users.noreply.github.com>
2022-09-25 10:06:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,8 +51,10 @@ function creativesCount() {
 
   leftSide.style.width = "100%";
   rightSide.style.display="none"
-  if (count < 20) {
+  if (count <= 20) {
     leftSideButton.style.visibility = "visible";
+    leftSideButton.disabled = false;
+    leftSideButton.style.boxShadow = "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px";
   }
   if (count === 20) {
     leftSideButton.style.visibility = "visible";
